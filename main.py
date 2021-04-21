c696ea1bf92eac8d78885984856acd90f456e9e1
Nils Strelow <strelow.nils@gmail.com>
2021-04-21 13:41:50 +0200

    refactor: Switch to FastAPI

---
+++ main.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from fastapi import FastAPI, Header
+from fastapi.middleware.cors import CORSMiddleware
+
+from generator import generate_repo
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def generate(
+    x_gitlab_token: Optional[str] = Header(None),
+    x_supabase_token: Optional[str] = Header(None),
+    x_study_id: Optional[str] = Header(None),
+):
+    generate_repo(x_gitlab_token, x_supabase_token, x_study_id)
+    return {'message': "Finished generating repository"}
